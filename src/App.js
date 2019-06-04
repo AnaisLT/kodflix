7cb9c9bce81f173f02ee221a68754e6d4cac62f2
AnaisLT <trope.anais@gmail.com>
2019-06-04 17:09:25 +0100

    Display TV show covers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import movieCover from './FilmCover.jpg';
+import Daredevil from './images/Daredevil.jpg';
+import Godless from './images/Godless.jpg';
+import Gotham from './images/Gotham.jpg';
+import Supergirl from './images/Supergirl.jpg';
+import Vikings from './images/Vikings.jpg';
+import Westworld from './images/Westworld.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={movieCover} alt='My Favourite Movie Cover'/>
+      <div className="container">
+        <div className="itemName">
+          <img src={Daredevil} alt='Daredevil cover' />
+        </div>
+        <div className="itemName">
+          <img src={Godless} alt='Godless cover' />
+        </div>
+        <div className="itemName">
+          <img src={Gotham} alt='Gotham cover' />
+        </div>
+      </div>
+      <div className="container">
+        <div className="itemName">
+          <img src={Supergirl} alt='Supergirl cover' />
+        </div>
+        <div className="itemName">
+          <img src={Vikings} alt='Vikings cover' />
+        </div>
+        <div className="itemName">
+          <img src={Westworld} alt='Westworld cover' />
+        </div>
+      </div>
     </div>
   );
 }
