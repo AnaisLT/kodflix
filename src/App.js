864cdc1a634e3f3a8fe2afe60812dc3aa3414104
AnaisLT <trope.anais@gmail.com>
2019-06-04 18:35:12 +0100

    Add TV show titles using flex

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
 import React from 'react';
-import Daredevil from './images/Daredevil.jpg';
-import Godless from './images/Godless.jpg';
-import Gotham from './images/Gotham.jpg';
-import Supergirl from './images/Supergirl.jpg';
-import Vikings from './images/Vikings.jpg';
-import Westworld from './images/Westworld.jpg';
+import MovieCover from './FilmCover.jpg';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <img src={MovieCover} alt='My favourite movie cover' />
       <div className="container">
-        <div className="itemName">
-          <img src={Daredevil} alt='Daredevil cover' />
-        </div>
-        <div className="itemName">
-          <img src={Godless} alt='Godless cover' />
-        </div>
-        <div className="itemName">
-          <img src={Gotham} alt='Gotham cover' />
-        </div>
+        <div className="itemName">Daredevil</div>
+        <div className="itemName">Godless</div>
+        <div className="itemName">Gotham</div>
       </div>
       <div className="container">
-        <div className="itemName">
-          <img src={Supergirl} alt='Supergirl cover' />
-        </div>
-        <div className="itemName">
-          <img src={Vikings} alt='Vikings cover' />
-        </div>
-        <div className="itemName">
-          <img src={Westworld} alt='Westworld cover' />
-        </div>
+        <div className="itemName">Supergirl</div>
+        <div className="itemName">Vikings</div>
+        <div className="itemName">Westworld</div>
       </div>
     </div>
   );
