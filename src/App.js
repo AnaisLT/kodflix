d609a29cf265067bf558384695b3739a92bc7cca
AnaisLT <trope.anais@gmail.com>
2019-06-05 10:53:11 +0100

    Change a TV cover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import MovieCover from './FilmCover.jpg';
-
+import React, { Component } from 'react';
+import Daredevil from './images/Daredevil.jpg';
+import Gotham from './images/Gotham.jpg';
+import StarTrek from './images/Star Trek.jpg';
+import Supergirl from './images/Supergirl.jpg';
+import Vikings from './images/Vikings.jpg';
+import Westworld from './images/Westworld.jpg';
 import './App.css';
 
-function App() {
+class App extends Component {
+render () {
   return (
     <div className="App">
-    <img src={MovieCover} alt='My favourite movie cover' />
       <div className="container">
-        <div className="itemName">Daredevil</div>
-        <div className="itemName">Godless</div>
-        <div className="itemName">Gotham</div>
+        <div className="itemName">
+          <img src={Daredevil} alt='Daredevil cover' />
+        </div>
+        <div className="itemName">
+          <img src={Gotham} alt='Gotham cover' />
+        </div>
+        <div className="itemName">
+          <img src={StarTrek} alt='Star Trek cover' />
+        </div>
       </div>
       <div className="container">
-        <div className="itemName">Supergirl</div>
-        <div className="itemName">Vikings</div>
-        <div className="itemName">Westworld</div>
+        <div className="itemName">
+          <img src={Supergirl} alt='Supergirl cover' />
+        </div>
+        <div className="itemName">
+          <img src={Vikings} alt='Vikings cover' />
+        </div>
+        <div className="itemName">
+          <img src={Westworld} alt='Westworld cover' />
+        </div>
       </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
