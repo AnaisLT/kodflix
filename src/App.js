4f5dda2a633d132a8a3f47f021dc5af02b4ead98
AnaisLT <trope.anais@gmail.com>
2019-06-05 12:34:25 +0100

    Add the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -8,34 +8,53 @@ import Westworld from './images/Westworld.jpg';
 import './App.css';
 
 class App extends Component {
-render () {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="itemName">
-          <img src={Daredevil} alt='Daredevil cover' />
+  render() {
+    return (
+      <div className="App">
+      <br /><br /><br />
+        <div className='container'>
+          <div className='itemName'>
+            <img src={Daredevil} alt='Daredevil cover' />
+          <div className='overlay'>
+            <h1>Daredevil</h1>
+          </div>
+          </div>
+          <div className='itemName'>
+            <img src={Gotham} alt='Gotham cover' />
+            <div className='overlay'>
+              <h1>Gotham</h1>
+            </div>
+          </div>
+          <div className='itemName'>
+            <img src={StarTrek} alt='Star Trek cover' />
+            <div className='overlay'>
+              <h1>Star Trek Discovery</h1>
+            </div>
+          </div>
         </div>
-        <div className="itemName">
-          <img src={Gotham} alt='Gotham cover' />
-        </div>
-        <div className="itemName">
-          <img src={StarTrek} alt='Star Trek cover' />
+        <div className='container'>
+          <div className='itemName'>
+            <img src={Supergirl} alt='Supergirl cover' />
+            <div className='overlay'>
+              <h1>Supergirl</h1>
+            </div>
+          </div>
+          <div className='itemName'>
+            <img src={Vikings} alt='Vikings cover' />
+            <div className='overlay'>
+              <h1>Vikings</h1>
+            </div>
+          </div>
+          <div className='itemName'>
+            <img src={Westworld} alt='Westworld cover' />
+            <div className='overlay'>
+              <h1>Westworld</h1>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="container">
-        <div className="itemName">
-          <img src={Supergirl} alt='Supergirl cover' />
-        </div>
-        <div className="itemName">
-          <img src={Vikings} alt='Vikings cover' />
-        </div>
-        <div className="itemName">
-          <img src={Westworld} alt='Westworld cover' />
-        </div>
-      </div>
-    </div>
-    );
+      );
+    }
   }
-}
-
-export default App;
+  
+  export default App;
