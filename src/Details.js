a4ddd987de7a9acabc6ea29ff9cf2a497b0bb9cc
AnaisLT <trope.anais@gmail.com>
2019-06-12 12:50:20 +0100

    Create state component to display delayed mesage.

--- src/Details.js
+++ src/Details.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
-  return (
-    <>
-    <h1>Welcome to the details page.</h1>
-      <p>Coming soon!</p>
-    <Link to='/'>
-      Back to the home page
-    </Link>
-    </>
-    )
+export default class Details extends Component {
+  constructor() {
+    super();
+    this.state = {
+      welcomeMessage: 'Welcome to the details page.'
+    };
+  }
+
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState ({
+        welcomeMessage: 'The best is yet to come!'
+      });
+    }, 3000);
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.welcomeMessage}</h1>
+        <p>Excuse our appearance while we get ready for the show.</p>
+        <Link to='/'>Back to the home page</Link>
+      </div>
+    );
+  }
 }
 
