90ff0d6f2d9e4a80d4aed348c1feb6870706255d
AnaisLT <trope.anais@gmail.com>
2019-06-13 12:52:16 +0100

    Redirect user to a "not-found" page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Details from './Details';
 import Gallery from './Gallery';
-
+import NotFound from './NotFound';
 import './App.css';
 
 class App extends Component {
@@ -10,8 +10,11 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+        <Switch>
         <Route exact path='/' component={Gallery} />
+        <Route exact path='/not-found'  component={NotFound}/>
         <Route exact path='/:tvshowId' component={Details} />
+        </Switch>
       </div>
       </Router>
     );
--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import getGallery from './gallery_get';
 
 export default class Details extends Component {
@@ -13,17 +13,22 @@ export default class Details extends Component {
   componentDidMount() {
     let tvshowId = this.props.match.params.tvshowId;
     let tvshow = getGallery()
-    .find((tvshow) => tvshow.id === tvshowId);
+      .find((tvshow) => tvshow.id === tvshowId);
     this.setState({ tvshow: tvshow });
   }
 
   render() {
-      return(
-          <div>
+    if (this.state.tvshow === undefined) {
+      return <Redirect to='/not-found' />;
+    } else {
+      return (
+        <div>
           <h1>{this.state.tvshow.name}</h1>
+          <p>More coming soon!</p>
+          <p>Excuse our appearance while we suit up for the show.</p>
           <Link to='/'>Back to the home page</Link>
-          </div >
+        </div >
       );
     }
   }
-
+}
