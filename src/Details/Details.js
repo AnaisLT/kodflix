6af16ef26371def1f22abfad758037550b953b0e
AnaisLT <trope.anais@gmail.com>
2019-06-19 11:56:43 +0100

    Add a synopsis for each show

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import getGallery from './gallery_get';
+import getGallery from '../gallery_get';
+import './Details.css'
 
 export default class Details extends Component {
   constructor() {
@@ -22,11 +23,21 @@ export default class Details extends Component {
       return <Redirect to='/not-found' />;
     } else {
       return (
-        <div>
-          <h1>{this.state.tvshow.name}</h1>
-          <p>More coming soon!</p>
-          <p>Excuse our appearance while we suit up for the show.</p>
-          <Link to='/'>Back to the home page</Link>
+        <div className='Details'>
+          <h1 className='title'>{this.state.tvshow.name}</h1>
+          <div className='content'>
+            <div className='synopsis'>
+              {this.state.tvshow.synopsis}
+            </div>
+            <div className='image'>
+              <img
+                src={this.state.tvshow.cover}
+                alt={this.state.tvshow.name} />
+            </div>
+            </div>
+          <button>
+          <Link to='/'>Home page</Link>
+          </button>
         </div >
       );
     }
--- src/NotFound.js
+++ src/NotFound.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
-        <div>
-            <h1>Ooops...sorry, the page doesn't exist :(</h1>
+        <div className='notfound'>
+            <div className='notfoundalert'>
+                <h1>Ooops...sorry, we haven't found the page you were looking for :(</h1>
+                <div className='text'>
+                    <p>Head back to the home page where all our heroes live.</p>
+                    <p>They'll take you on an unforgettable journey!</p>
+                </div>
+            </div>
+            <div>
+                <button className='backhome'>
+                    <Link to='/'>Home page</Link>
+                </button>
+            </div>
         </div>
-    )
-}
+    );
+} 
