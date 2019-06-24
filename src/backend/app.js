ec7e5368b0ab43771b73c5afa604e806a8cbd811
AnaisLT <trope.anais@gmail.com>
2019-06-24 17:05:01 +0100

    Return a list of shows from the backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const getShows = require('./tvshows');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/tvshows', (req, res) => res.send(getShows()));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/backend/tvshows.js
@@ -0,0 +1,35 @@
+module.exports = function getShows() {
+    return [{
+        id: 'daredevil',
+        title: 'Daredevil',
+        synopsis: 'Blinded as a boy but gifted with heightened senses, Matt Murdock fights injustice by day as lawyer and by night as Daredevil in Hell\'s Kitchen, New York City.'
+    }, {
+        id: 'gotham',
+        title: 'Gotham',
+        synopsis: 'Rookie cop James Gordon takes on Gotham City crime and corruption to avenge the murder of Bruce Wayne\'s parents.'
+    }, {
+        id: 'star-trek-discovery',
+        title: 'Star Trek Discovery',
+        synopsis: 'After a century of silence, war erupts between the Federation and Klingon Empire, with a disgraced Starfleet officer at the center of the conflict.'
+    }, {
+        id: 'supergirl',
+        title: 'Supergirl',
+        synopsis: 'Journalist-by-day Kara Danvers leads a double life as Supergirl. With the help of her loyal friends and her foster sister, Alex, she uses her superpowers to protect National City against human and alien threats alike...'
+    }, {
+        id: 'vikings',
+        title: 'Vikings',
+        synopsis: 'Vikings follows the adventures of historical hero Ragnar Lothbrok and his family. Determined to become King and honour the Gods, he will stop at nothing to conquer new lands.'
+    }, {
+        id: 'westworld',
+        title: 'Westworld',
+        synopsis:'In a Wild West fantasy park staffed by artificial hosts, guests find themselves confronted to a dangerous world of infinite possibilities...'
+    }, {
+        id: 'jessica-jones',
+        title: 'Jessica Jones',
+        synopsis: 'Haunted by a traumatic past, she\'s also a gifted PI with super abilities... and quite a temperament.'
+    }, {
+        id: 'good-omens',
+        title: 'Good Omens',
+        synopsis: 'The End of the World is coming, which means a fussy Angel and a loose-living Demon who\'ve become overly fond of life on Earth are forced to form an unlikely alliance to stop Armageddon.'
+    }];
+};
