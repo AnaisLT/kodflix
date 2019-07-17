e8ef14f8b13ae742b9ce6cd5ae3b72ecee09a17b
AnaisLT <trope.anais@gmail.com>
2019-07-17 16:49:57 +0100

    Testing

--- src/backend/app.js
+++ src/backend/app.js
@@ -6,9 +6,9 @@ const getShows = require('./tvshows');
 
 app.get('/rest/tvshows', (req, res) => res.send(getShows()));
 
-app.use(express.static(path.join(_dirname, '../../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(_dirname, '../../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
