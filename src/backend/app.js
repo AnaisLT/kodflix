89129c55d81235e82726b00308431a57c2cbc2d1
AnaisLT <trope.anais@gmail.com>
2019-07-17 16:12:08 +0100

    Enable the backend server in production

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const getShows = require('./tvshows');
 
 app.get('/rest/tvshows', (req, res) => res.send(getShows()));
+
+app.use(express.static(path.join(_dirname, '../../build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(_dirname, '../../build', 'index.html'));
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
